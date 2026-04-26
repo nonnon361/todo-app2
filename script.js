b7bf41b0129b7cd9fa9e8c7c95d86a082714eb18
nonnon361 <127860110+nonnon361@users.noreply.github.com>
2026-04-26 01:40:46 +0000

    add drag position helper

--- script.js
+++ script.js
@@ -109,3 +109,17 @@ list.addEventListener("dragover", (e) => {
     list.insertBefore(dragging, afterElement);
   }
 });
+function getAfterElement(y) {
+  const elements = [...list.querySelectorAll("li:not(.dragging)")];
+
+  return elements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
+
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child };
+    } else {
+      return closest;
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
