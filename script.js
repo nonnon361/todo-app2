944ba10152d46708b9ad72fd1296a4c7ce96e52f
nonnon361 <127860110+nonnon361@users.noreply.github.com>
2026-04-26 01:41:28 +0000

    refactor create task function

--- script.js
+++ script.js
@@ -123,3 +123,37 @@ function getAfterElement(y) {
     }
   }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
+function createTask(text, done = false) {
+  const li = document.createElement("li");
+  li.textContent = text;
+
+  if (done) li.classList.add("done");
+
+  li.setAttribute("draggable", "true");
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("done");
+    saveTasks();
+  });
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "X";
+
+  delBtn.addEventListener("click", () => {
+    li.remove();
+    saveTasks();
+  });
+
+  li.appendChild(delBtn);
+
+  li.addEventListener("dragstart", () => {
+    li.classList.add("dragging");
+  });
+
+  li.addEventListener("dragend", () => {
+    li.classList.remove("dragging");
+    saveTasks();
+  });
+
+  return li;
+}
