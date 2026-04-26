804e1be01f98fee08b4354dedc9ec62cce56ca5b
nonnon361 <127860110+nonnon361@users.noreply.github.com>
2026-04-26 01:11:54 +0000

    add click event

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const input = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const list = document.getElementById("taskList");
+button.addEventListener("click", () => {
+  console.log("clicked");
+});
