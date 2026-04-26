c0d0f5ca58367a1647388035ab83f30cd4970e9f
nonnon361 <127860110+nonnon361@users.noreply.github.com>
2026-04-26 01:13:17 +0000

    prevent empty input

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const input = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const list = document.getElementById("taskList");
 button.addEventListener("click", () => {
+  if (input.value === "") return;
+
   const li = document.createElement("li");
   li.textContent = input.value;
   list.appendChild(li);
+
+  input.value = "";
 });
-input.value = "";
