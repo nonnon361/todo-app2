f54a4f55ca0c43323070e78248c54045f94540d2
nonnon361 <127860110+nonnon361@users.noreply.github.com>
2026-04-26 01:12:28 +0000

    create task

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const input = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const list = document.getElementById("taskList");
 button.addEventListener("click", () => {
-  console.log("clicked");
+  const li = document.createElement("li");
+  li.textContent = input.value;
+  list.appendChild(li);
 });
