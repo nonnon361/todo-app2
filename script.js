1ea6dfd0c7e4dfb70ed696a3fbd57c4ddee3c318
nonnon361 <127860110+nonnon361@users.noreply.github.com>
2026-04-26 01:24:45 +0000

    toggle dark mode

--- script.js
+++ script.js
@@ -58,3 +58,8 @@ function loadTasks() {
   });
 }
 loadTasks();
+const darkBtn = document.getElementById("darkBtn");
+
+darkBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
